aa31de944c84eaf40fc97046bf72299508b92ed3
Jorge Gajon <gajon@gajon.org>
2012-05-31 16:23:05 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AventonesLanding::Application.config.secret_token = 'd753265ccb8662be0943060e518e766f66ebba66b3f4ad5e44e3971571e8f257a73e99c9cd1b0fee1290e73330579f66c31967e8e768b82117725b66d2d338ff'
