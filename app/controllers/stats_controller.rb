0c776716563770d9e80943368686d73deb8a0abb
Jorge Gajon <gajon@gajon.org>
2012-06-12 23:38:18 -0500

    HTTP Digest Auth

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,20 @@
 class StatsController < ApplicationController
   layout 'stats'
 
+  USERS = { "lifo" => "world" }
+
+  before_filter :authenticate
+
   def index
     @surveys = Survey.all
+  end
+
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
   end
 end
